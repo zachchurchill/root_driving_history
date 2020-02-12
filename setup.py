078a6e251fa32d704e5249d4f4e3446174616386
Zach Churchill <zachurchill@protonmail.com>
2020-02-11 19:15:42 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="root_driving_history",
+    version="0.1",
+    author="applicant",
+    author_email="applicant@joinroot.com",
+    description="Package for parsing and summarizing driving history data.",
+    packages=["root_driving_history"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
+    ],
+)
